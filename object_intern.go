b749c9d43bf26f1fc63ee8230b26989e5ea504ca
Robert Milan <robert.p.milan@gmail.com>
2019-01-09 19:40:07 -0300

    add initial layout

---
+++ object_intern.go
@@ -0,0 +1,10 @@
+package goi
+
+type ObjectIntern struct {
+	Objects map[uint8][]uintptr
+}
+
+// AddOrGet finds or adds and then returns a uintptr to an object
+func (o *ObjectIntern) AddOrGet([]string) uintptr {
+	return 0
+}
