41320fc4e8c0713a92d4a61b683c0b319c75444e
Robert Milan <robert.p.milan@gmail.com>
2019-01-12 19:21:50 -0300

    add benchmarks for de/compress

--- object_intern_test.go
+++ object_intern_test.go
@@ -19,7 +19,8 @@ func TestCompressionDecompress(t *testing.T) {
 		c := oi.Compress(b)
 		d, err := oi.Decompress(c)
 		if err != nil {
-			continue
+			t.Error("Decompression failed for: ", c)
+			return
 		}
 		testResults = append(testResults, d)
 	}
@@ -27,9 +28,58 @@ func TestCompressionDecompress(t *testing.T) {
 	for i, res := range testResults {
 		for k, v := range res {
 			if v != testBytes[i][k] {
-				t.Error("Mismatched")
+				t.Error("Mismatched: ", v, " - ", testBytes[i][k])
 				return
 			}
 		}
+	}
+}
+
+func BenchmarkCompressShoco(b *testing.B) {
+	cnf := NewConfig()
+	cnf.CompressionType = SHOCO
+	benchmarkCompress(b, cnf)
+}
+
+func BenchmarkDecompressShoco(b *testing.B) {
+	cnf := NewConfig()
+	cnf.CompressionType = SHOCO
+	benchmarkDecompress(b, cnf)
+}
+
+func BenchmarkCompressNone(b *testing.B) {
+	cnf := NewConfig()
+	cnf.CompressionType = 0
+	benchmarkCompress(b, cnf)
+}
+
+func BenchmarkDecompressNone(b *testing.B) {
+	cnf := NewConfig()
+	cnf.CompressionType = 0
+	benchmarkDecompress(b, cnf)
+}
+
+func benchmarkCompress(b *testing.B, cnf *ObjectInternConfig) {
+	oi := NewObjectIntern(cnf)
+	data := []byte("HowTheWindBlowsThroughTheTrees")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		oi.Compress(data)
+	}
+}
+
+func benchmarkDecompress(b *testing.B, cnf *ObjectInternConfig) {
+	oi := NewObjectIntern(cnf)
+	data := []byte("HowTheWindBlowsThroughTheTrees")
+	comp := oi.Compress(data)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		oi.Decompress(comp)
 	}
 }
